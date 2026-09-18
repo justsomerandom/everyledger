f28077f09433b862ab6f07e404e9ed80ba6695ea
abdurrahman-l7 <abdurrahman@l7it.net>
2026-09-18 14:39:37 +0200

    Spring project setup

---
+++ src/main/java/com/everyledger/EveryledgerApplication.java
@@ -0,0 +1,12 @@
+package com.everyledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryledgerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EveryledgerApplication.class, args);
+  }
+}
